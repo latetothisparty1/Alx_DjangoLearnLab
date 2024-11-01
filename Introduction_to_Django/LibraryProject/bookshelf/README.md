d5ee6d9217bd1e5fdb0c66fed7988f695406446d
Akram Mathu <mathuakram@gmail.com>
2024-11-01 16:57:20 +0300

    Sixth Commit

---
+++ Introduction_to_Django/LibraryProject/bookshelf/README.md
@@ -0,0 +1,12 @@
+# Bookshelf App Admin Integration
+
+## Overview
+This document outlines the steps taken to integrate the Book model with the Django admin interface for better management and visibility of book data.
+
+## Steps to Integrate
+
+1. **Navigate to the App Directory**:
+   ```bash
+   cd LibraryProject/bookshelf
+
+s
--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Book 
 
 # Register your models here.
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')  # Customize which fields to display
+    search_fields = ('title', 'author__name')  # Enable searching by title and author's name
+    list_filter = ('publication_year',)  # Enable filtering by publication year
