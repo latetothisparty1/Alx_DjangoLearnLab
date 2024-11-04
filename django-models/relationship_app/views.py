c5b9b29e41a6fe2074d791454a9fb08b01a9a63c
Akram Mathu <mathuakram@gmail.com>
2024-11-04 15:19:22 +0300

    Sixth

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -8,16 +8,12 @@ from django.shortcuts import render
 from .models import Book
 from .models import Library
 
-def list_books(request):
-    books = Book.objects.all()
-    return render(request, 'relationship_app/list_books.html', {'books': books})
-
-# relationship_app/views.py
-
-
-
-
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
