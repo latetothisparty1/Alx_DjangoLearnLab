ba1969a96b2593aa659ae7a5d96b90cf28e340d4
Akram Mathu <mathuakram@gmail.com>
2024-11-07 15:47:12 +0300

    012

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,9 +1,11 @@
 # relationship_app/views.py
 
+from django.views.generic.detail import DetailView
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView, LogoutView
+
 
 # User Registration View
 def register(request):
