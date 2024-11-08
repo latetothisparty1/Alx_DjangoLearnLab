f2c7b8be093c162839ef4d461b432d04c0501d07
Akram Mathu <mathuakram@gmail.com>
2024-11-08 09:29:16 +0300

    017

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,6 +1,7 @@
 # relationship_app/urls.py
 from django.urls import path
 from .views import list_books, LibraryDetailView, user_login, user_logout, user_register
+from .views import admin_view, librarian_view, member_view
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),
@@ -8,5 +9,9 @@ urlpatterns = [
     path('login/', user_login, name='login'),  # URL for login
     path('logout/', user_logout, name='logout'),  # URL for logout
     path('register/', user_register, name='register'),  # URL for registration
+    path('admin/', admin_view, name='admin_view'),  # URL for Admin view
+    path('librarian/', librarian_view, name='librarian_view'),  # URL for Librarian view
+    path('member/', member_view, name='member_view'),  # URL for Member view
 ]
+
 
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -9,6 +9,9 @@ from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 
+from django.contrib.auth.decorators import user_passes_test
+from .models import UserProfile
+
 
 def list_books(request):
     books = Book.objects.all()  # Fetch all book records from the database
@@ -49,3 +52,25 @@ def user_register(request):
     else:
         form = UserCreationForm()
     return render(request, 'relationship_app/register.html', {'form': form})
+
+
+def is_admin(user):
+    return user.is_authenticated and user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return user.is_authenticated and user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return user.is_authenticated and user.userprofile.role == 'Member'
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
