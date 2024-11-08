cc02cf8088f83b05385afc4de0329354e81e08de
Akram Mathu <mathuakram@gmail.com>
2024-11-08 09:15:20 +0300

    017

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,8 +1,12 @@
 # relationship_app/urls.py
 from django.urls import path
-from .views import list_books, LibraryDetailView
+from .views import list_books, LibraryDetailView, user_login, user_logout, user_register
 
 urlpatterns = [
-    path('books/', list_books, name='list_books'),  # URL for listing books
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # URL for library details
+    path('books/', list_books, name='list_books'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('login/', user_login, name='login'),  # URL for login
+    path('logout/', user_logout, name='logout'),  # URL for logout
+    path('register/', user_register, name='register'),  # URL for registration
 ]
+
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -4,6 +4,11 @@ from django.views.generic.detail import DetailView
 from .models import Book
 from .models import Library
 
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+
 
 def list_books(request):
     books = Book.objects.all()  # Fetch all book records from the database
@@ -13,3 +18,34 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('list_books')  # Redirect to a page after login
+        else:
+            return render(request, 'relationship_app/login.html', {'error': 'Invalid credentials'})
+    return render(request, 'relationship_app/login.html')
+
+def user_logout(request):
+    logout(request)
+    return render(request, 'relationship_app/logout.html')
+
+def user_register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('list_books')  # Redirect to a page after registration
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
