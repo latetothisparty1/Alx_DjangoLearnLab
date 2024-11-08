6fb06bbe2ae13193496154ab382b1ea520520c1f
Akram Mathu <mathuakram@gmail.com>
2024-11-08 09:35:33 +0300

    018

--- django-models/relationship_app/admin.py
+++ django-models/relationship_app/admin.py
@@ -1,3 +1,10 @@
+# relationship_app/admin.py
 from django.contrib import admin
+from .models import UserProfile
 
-# Register your models here.
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'role')  # Display user and role in the admin list view
+    search_fields = ('user__username',)  # Allow searching by username
+
+# Register the UserProfile model with the admin site
+admin.site.register(UserProfile, UserProfileAdmin)
