ee60e7cabd2efdc495e6cd2857b0915ea650839f
Akram Mathu <mathuakram@gmail.com>
2024-11-24 15:52:12 +0300

    001

---
+++ api_project/api/models.py
@@ -0,0 +1,11 @@
+# api/models.py
+
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+
