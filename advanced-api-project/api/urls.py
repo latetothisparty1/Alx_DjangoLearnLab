b0f6a5257f1d606c727bbda5bb133676718727d4
Akram Mathu <mathuakram@gmail.com>
2024-11-27 11:43:37 +0300

    014

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -7,3 +7,8 @@ urlpatterns = [
     path('books/', BookListView.as_view(), name='book-list'),  # List and create books
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),  # Retrieve, update, delete a book
 ]
+
+books/create
+books/update
+books/delete
+
