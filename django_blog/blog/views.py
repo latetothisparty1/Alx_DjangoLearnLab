3378b59f71bf427069a7731cb9dacbb9adaf684a
Akram Mathu <mathuakram@gmail.com>
2024-12-06 11:37:41 +0300

    003

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
-
 # Create your views here.
+
+# blog/views.py
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
+from .forms import CustomUser CreationForm
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomUser CreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('profile')
+    else:
+        form = CustomUser CreationForm()
+    return render(request, 'blog/register.html', {'form': form})
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('profile')
+        else:
+            return render(request, 'blog/login.html', {'error': 'Invalid credentials'})
+    return render(request, 'blog/login.html')
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')
+
+@login_required
+def profile(request):
+    return render(request, 'blog/profile.html', {'user': request.user})
+
--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -124,5 +124,3 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-["USER", "PORT"]
-
