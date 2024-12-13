ba60120b4f9bab47ec2ccab0a626f79d3363ebb7
Akram Mathu <mathuakram@gmail.com>
2024-12-13 10:40:27 +0300

    002

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -13,3 +13,4 @@ class UserSerializer(serializers.ModelSerializer):
         user.save()
         return user
 
+from rest_framework.authtoken.models import Token", "serializers.CharField()", "Token.objects.create", "get_user_model().objects.create_user"
